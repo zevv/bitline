011340e514e3eb617b5d264fac59a048c00bf1eb
Ico Doornekamp <ico@zevv.nl>
2021-08-09 18:38:10 +0200

    cleanup nsec

--- src/view.nim
+++ src/view.nim
@@ -157,13 +157,13 @@ proc drawGrid(v: View) =
 
   template aux(interval: Time,
                iyear: int, imonth: Month, imday: MonthdayRange,
-               ihour, imin, isec: int, insec: int,
+               ihour, imin, isec: int,
                fmt1, fmt2: string,
                code: untyped) {.dirty.} =
 
     if tpp * 10 < interval:
       var (year, month, mday)= (iyear.int, imonth.int, imday.int)
-      var (hour, min, sec, nsec) = (ihour.int, imin.int, isec.int, insec.int)
+      var (hour, min, sec, nsec) = (ihour.int, imin.int, isec.int, 0)
       var nLabels = 0
       while true:
         let t = initDateTime(mday.MonthDayRange, month.Month, year, hour, min, sec, nsec, utc())
@@ -191,40 +191,29 @@ proc drawGrid(v: View) =
 
         code
 
-        if nsec >= 1000000000:
-          inc sec
-          nsec = 0
-        if sec >= 60:
-          inc min
-          sec = 0
-        if min >= 60:
-          inc hour
-          min = 0
-        if hour >= 24:
-          hour = 0
-          inc mday
-        if month < 13 and mday > getDaysInMonth(month.Month, year):
-          mday = 1
-          inc month
-        if month > 12:
-          month = 1
-          inc year
+        if nsec >= 1000000000: (inc sec; nsec = 0)
+        if sec >= 60: (inc min; sec = 0)
+        if min >= 60: (inc hour; min = 0)
+        if hour >= 24: (hour = 0; inc mday)
+        if month < 13 and mday > getDaysInMonth(month.Month, year): (mday = 1; inc month)
+        if month > 12: (month = 1; inc year)
+
       if nLabels > 0:
         dy += v.cfg.rowSize
 
-  let t = v.cfg.ts.lo.fromUnixFloat.utc
-  aux(0.001,        t.year, t.month, t.monthday, t.hour, t.minute, t.second, 0, "fff", "ss'.'fff"): inc nsec,   1 * 1000 * 1000
-  aux(0.01,         t.year, t.month, t.monthday, t.hour, t.minute, t.second, 0, "fff", "ss'.'fff"): inc nsec,  10 * 1000 * 1000
-  aux(0.1,          t.year, t.month, t.monthday, t.hour, t.minute, t.second, 0, "fff", "ss'.'fff"): inc nsec, 100 * 1000 * 1000
-  aux(1,            t.year, t.month, t.monthday, t.hour, t.minute, t.second, 0, "ss", "HH:mm:ss"): inc sec
-  aux(10,           t.year, t.month, t.monthday, t.hour, t.minute, 0,        0, "ss", "HH:mm:ss"): inc sec, 10
-  aux(60,           t.year, t.month, t.monthday, t.hour, t.minute, 0,        0, "mm", "HH:mm:ss"): inc min
-  aux(60*10,        t.year, t.month, t.monthday, t.hour, 0,        0,        0, "mm", "HH:mm"): inc min, 10
-  aux(60*60,        t.year, t.month, t.monthday, t.hour, 0,        0,        0, "HH", "HH:mm"): inc hour
-  aux(60*60*6,      t.year, t.month, t.monthday, 0,      0,        0,        0, "HH", "HH:mm"): inc hour, 6
-  aux(60*60*24,     t.year, t.month, t.monthday, 0,      0,        0,        0, "dd", "MMM dd"): inc mday
-  aux(60*60*24*30,  t.year, t.month, 1,          0,      0,        0,        0, "MMM", "MMM '`'yy"): inc month
-  aux(60*60*24*365, t.year, mJan,    1,          0,      0,        0,        0, "yy", "yyyy"): inc year
+  var t = v.cfg.ts.lo.fromUnixFloat.utc
+  aux(0.001,        t.year, t.month, t.monthday, t.hour, t.minute, t.second, "fff", "s'.'fff"): inc nsec,   1 * 1000 * 1000
+  aux(0.01,         t.year, t.month, t.monthday, t.hour, t.minute, t.second, "fff", "s'.'fff"): inc nsec,  10 * 1000 * 1000
+  aux(0.1,          t.year, t.month, t.monthday, t.hour, t.minute, t.second, "fff", "s'.'fff"): inc nsec, 100 * 1000 * 1000
+  aux(1,            t.year, t.month, t.monthday, t.hour, t.minute, t.second, "ss", "HH:mm:ss"): inc sec
+  aux(10,           t.year, t.month, t.monthday, t.hour, t.minute, 0,        "ss", "HH:mm:ss"): inc sec, 10
+  aux(60,           t.year, t.month, t.monthday, t.hour, t.minute, 0,        "mm", "HH:mm:ss"): inc min
+  aux(60*10,        t.year, t.month, t.monthday, t.hour, 0,        0,        "mm", "HH:mm"): inc min, 10
+  aux(60*60,        t.year, t.month, t.monthday, t.hour, 0,        0,        "HH", "HH:mm"): inc hour
+  aux(60*60*6,      t.year, t.month, t.monthday, 0,      0,        0,        "HH", "HH:mm"): inc hour, 6
+  aux(60*60*24,     t.year, t.month, t.monthday, 0,      0,        0,        "dd", "MMM dd"): inc mday
+  aux(60*60*24*30,  t.year, t.month, 1,          0,      0,        0,        "MMM", "MMM '`'yy"): inc month
+  aux(60*60*24*365, t.year, mJan,    1,          0,      0,        0,        "yy", "yyyy"): inc year
 
 proc drawCursor(v: View) =
   let
